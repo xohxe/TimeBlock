3927cf0add4b7dd86a2c6ffed9dacd23a18ac6ea
xohxe <saeyan2477@gmail.com>
2023-11-03 14:41:31 +0900

    Initial Commit

---
+++ TimeBlock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeBlock
+//
+//  Created by 김소혜 on 11/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeBlock/TimeBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBlockApp.swift
+//  TimeBlock
+//
+//  Created by 김소혜 on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
