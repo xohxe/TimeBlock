69520450c694cbff298e6cd4400d084d78cbeb9f
xohxe <saeyan2477@gmail.com>
2023-12-19 17:02:00 +0900

    [Feat]: ObservedObject 프로토콜을 이용한 View 이동
    - Navigation View를 사용하지 않고 @ObservedObject 프로토콜을 사용하여 페이지간 탐색을 구현하였습니다.
    - ViewRouter에 현재 페이지 값 설정하였습니다.

--- TimeBlock/TimeBlockApp.swift
+++ TimeBlock/TimeBlockApp.swift
@@ -5,13 +5,20 @@
 //  Created by 김소혜 on 11/3/23.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
 struct TimeBlockApp: App {
+   // @StateObject var sheetMangager = SheetManager()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView(viewRouter: ViewRouter())
+//                .environmentObject(sheetMangager)
         }
+        .modelContainer(for: ToDo.self)
     }
 }
+
+
